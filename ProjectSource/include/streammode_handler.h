711d46e83b0d7433a7544f3972318d0f41dfc984
HeronSystems <dempsy@dodo.(none)>
2014-10-09 16:21:57 -0400

    stream mode handler forgotten files

---
+++ ProjectSource/include/streammode_handler.h
@@ -0,0 +1,11 @@
+#ifndef STREAMMODE_HANDLER_H
+#define STREAMMODE_HANDLER_H
+
+#include "StreamMode_Handler_global.h"
+
+class STREAMMODE_HANDLERSHARED_EXPORT StreamMode_Handler {
+public:
+    StreamMode_Handler();
+};
+
+#endif // STREAMMODE_HANDLER_H
